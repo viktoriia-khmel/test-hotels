e5c304611f77e30e68fad60239a34fb9b5af93e7
Viktoriia Khmel <legchilova.viktoria@gmail.com>
2024-11-29 10:03:23 +0300

    add computers fn

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,21 @@
 "use strict";
 
-function makeTransaction(quantity, pricePerDroid) {
-  return `You ordered ${quantity} droids worth ${
-    quantity * pricePerDroid
-  } credits!`;
+function getComputers(num) {
+  const absNum = Math.abs(num);
+  const nElevenToFourteen = absNum % 100;
+  const nOneToFour = absNum % 10;
+
+  if (nElevenToFourteen >= 11 && nElevenToFourteen <= 14) {
+    return `${num} компьютеров`;
+  } else if (nOneToFour === 1) {
+    return `${num} компьютер`;
+  } else if (nOneToFour >= 2 && nOneToFour <= 4) {
+    return `${num} компьютера`;
+  } else return `${num} компьютеров`;
 }
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(getComputers(1));
+console.log(getComputers(12));
+console.log(getComputers(100));
+console.log(getComputers(227));
+console.log(getComputers(-48));
